7c94ca2763dd36c69aa1a4ff145f1d9bcf96e659
MelinaLushaku <63053349+MelinaLushaku@users.noreply.github.com>
2021-05-30 18:47:25 +0200

    Initial commit

---
+++ src/test/java/com/example/mednotes4/Mednotes4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mednotes4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mednotes4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
