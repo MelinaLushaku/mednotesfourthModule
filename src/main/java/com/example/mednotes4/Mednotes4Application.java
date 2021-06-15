683ba0328a40a27943f2d1d739ff6d116260017e
MelinaLushaku <63053349+MelinaLushaku@users.noreply.github.com>
2021-06-16 00:54:19 +0200

    ApiGateway

--- src/main/java/com/example/mednotes4/Mednotes4Application.java
+++ src/main/java/com/example/mednotes4/Mednotes4Application.java
@@ -2,8 +2,10 @@ package com.example.mednotes4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Mednotes4Application {
 
     public static void main(String[] args) {
